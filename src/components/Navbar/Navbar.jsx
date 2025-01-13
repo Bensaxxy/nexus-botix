607482b1971a1df7dcbf2677cd4324c1e8c6d4cf
Bensaxxy <benjaminsegun38@gmail.com>
2025-01-13 19:15:21 +0100

    change the navbar bg color

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className=" bg-gray2 w-full">
-      <div className=" bg-gray2 w-full fixed z-50 ">
+    <div className=" bg-gray3 w-full">
+      <div className=" bg-gray3 w-full fixed z-50 ">
         <div className=" container flex items-center justify-between py-4">
           <Link to="/">
             <img src="/Logo.svg" alt="Logo" className="h-10" />
